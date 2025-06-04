d24b1604d85fa6dc8ef2a236dc23d95593ca6452
unknown <prakashnnpatel@gmail.com>
2025-06-04 13:58:19 +0530

    changes

---
+++ resources/views/contact-us.blade.php
@@ -0,0 +1,32 @@
+<div class="card">
+	<div class="card-body">
+		<h2 class="text-center mb-4">Contact Us</h2>
+		<p class="text-center">Have an idea for a new tool? Let's bring it to life - send us a message! We'll build and publish it here for public use.</p>
+		<p class="text-center">Your one idea could help or even change the lives of many people. </p>
+		<div class="row justify-content-center g-4">
+			<div class="col-md-4">
+				<div class="text-center p-4 border rounded-3 shadow-sm h-100">
+					<div class="mb-3">
+						<i class="fas fa-phone fa-2x text-primary"></i>
+					</div>
+					<h5>Phone</h5>
+					<p class="mb-0">(+91) 932 844 8634</p>
+				</div>
+			</div>
+
+			<div class="col-md-4">
+				<div class="text-center p-4 border rounded-3 shadow-sm h-100">
+					<div class="mb-3">
+						<i class="fas fa-envelope fa-2x text-primary"></i>
+					</div>
+					<h5>Email</h5>
+					<p class="mb-0">
+						<a href="mailto:pramukhtech.services@gmail.com" class="text-decoration-none">
+							pramukhtech.services@gmail.com
+						</a>
+					</p>
+				</div>
+			</div>
+		</div>
+    </div>
+</div>
