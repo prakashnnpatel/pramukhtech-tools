f4a86dd00d0e365a2c0209bd15e52c384ba5d654
unknown <prakashnnpatel@gmail.com>
2025-06-05 20:05:54 +0530

    Changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,9 @@
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
     @vite('resources/sass/frontend.scss')
     @stack('page_css')
+	@if(config('app.env') == 'production')
+		<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7987011474307455" crossorigin="anonymous"></script>
+	@endif
 </head>
 <body>
     @include('section.sidebar')
