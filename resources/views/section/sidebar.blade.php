1116cc85b99bdb28dd087f0998d3a8a311d250a9
unknown <hasamukhchaudhary3@gmail.com>
2025-06-11 18:57:48 +0530

    CHnages

--- resources/views/section/sidebar.blade.php
+++ resources/views/section/sidebar.blade.php
@@ -1,30 +1,3 @@
-{{--<div class="sidebar" id="sidebar-menu"> 
-    <ul class="list-categories">
-        <li>
-            <a class="menu-title-link" href="javascript:void(0);" class="menu-title-link active" title="Formatters">
-                <i class="fas fa-calculator menu-icn"></i><span>Calculator</span>
-            </a>
-            <ul class="sub-menu list-unstyled">
-                <li><a href="{{route('toollist', 'fd-calculator')}}" title="FD Calculator">FD Calculator</a></li>
-                <li><a href="{{route('toollist', 'sip-calculator')}}" title="SIP Calculator">SIP Calculator</a></li>
-                <li><a href="{{route('toollist', 'emi-calculator')}}" title="EMI Calculator">EMI Calculator</a></li>						
-            </ul>
-        </li>
-        <li>
-            <a class="menu-title-link" href="javascript:void(0);" class="menu-title-link active" title="Formatters">
-                <i class="fas fa-clock menu-icn"></i><span>TimeZone</span>
-            </a>
-            <ul class="sub-menu list-unstyled">
-                <li><a href="{{route('toollist', 'timezone')}}" title="UTC to IST">UTC to IST</a></li>
-                <li><a href="{{route('toollist', 'timezone')}}" title="IST to EST">IST to EST</a></li>
-                <li><a href="{{route('toollist', 'timezone')}}" title="UTC to EST">UTC to EST</a></li>
-                <li><a href="{{route('toollist', 'timezone')}}" title="UTC to CST">UTC to CST</a></li>
-                <li><a href="{{route('toollist', 'timezone')}}" title="GMT to PST">GMT to PST</a></li>
-            </ul>
-        </li>
-    </ul>
-</div>--}}
-
 <div class="sidebar">
   <div> 
     <span class="sidebar-toggle" title="Toggle Sidebar">
@@ -58,7 +31,7 @@
         </a>
       </li>
 	  <li class="has-submenu open">
-        <a href="javascript:void(0)" class="submenu-toggle">
+        <a href="#" class="submenu-toggle" onclick="event.preventDefault();">
           <i class="fas fa-calculator"></i>Calculators
           <i class="fas fa-chevron-right arrow"></i>
         </a>
