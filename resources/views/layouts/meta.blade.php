e864740e9e0d3974e28c5ca153291d6c52ee2a8b
unknown <prakashnnpatel@gmail.com>
2025-08-05 21:07:51 +0530

    Changes

--- resources/views/layouts/meta.blade.php
+++ resources/views/layouts/meta.blade.php
@@ -5,7 +5,6 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <meta name="author" content="{{ config('app.name') }}">
-<link rel="canonical" href="{{url()->current()}}"/>
 <meta property="og:locale" content="en_US" />
 <meta property="og:type" content="website" />
 <meta property="og:url" content="{{url()->current()}}" />
@@ -25,6 +24,7 @@
 			<meta name="description" content="The timezone converter tool is powerful & accurate time management, Convert Timezones Instantly and Schedule Meetings Globally with Ease." />
 			<meta property="og:title" content="Smart Timezone Converter | ToolHubSpot" />
 			<meta property="og:description" content="Easily convert and compare time zones worldwide with ToolHubSpot's tool. Instantly check the current time across cities, plan meetings, and avoid confusion!" />
+			<link rel="canonical" href="{{url()->current()}}"/>
 		@break
 
 		@case('emi-calculator')
@@ -32,13 +32,15 @@
 			<meta name="description" content="Use our EMI Calculator to calculate your loan EMIs with ease quickly. Get detailed insights into monthly payments, interest rates, and repayment schedules." />
 			<meta property="og:title" content="Free EMI Calculator - Calculate Loan EMIs | ToolHubSpot" />
 			<meta property="og:description" content="Easily calculate your monthly EMI with ToolHubSpot's Free EMI Calculator. Get quick and accurate results for home, car, personal, or business loans." />
+			<link rel="canonical" href="{{url('/emi-calculator')}}"/>
 		@break
 
 		@case('sip-calculator')
 			<title>The Smart SIP Calculator to Maximize Your Investments</title>
 			<meta name="description" content="SIP Calculator - Systematic Investment Plan Calculator. This tool can help you determine the amount you want to invest and the estimated value of the returns." />
-			<meta property="og:title" content="Free SIP Calculator – Plan Your Investments | ToolHubSpot" />
+			<meta property="og:title" content="Free SIP Calculator - Plan Your Investments | ToolHubSpot" />
 			<meta property="og:description" content="Plan your investments with ToolHubSpot's Free SIP Calculator. Instantly calculate expected returns from Systematic Investment Plans. Easy, accurate!" />
+			<link rel="canonical" href="{{url('/sip-calculator')}}"/>
 		@break
 
 		@case('fd-calculator')
@@ -46,6 +48,7 @@
 			<meta name="description" content="Calculate fixed deposit interest easily and maturity value with {{ config('app.name') }} FD Calculator. Plan your savings efficiently with accurate results." />
 			<meta property="og:title" content="Free FD Calculator - Calculate FD Interest | ToolHubSpot" />
 			<meta property="og:description" content="Use ToolHubSpot's Free FD Calculator to estimate fixed deposit returns. Quick, accurate, and simple - check maturity amounts and interest earned in seconds." />
+			<link rel="canonical" href="{{url('/fd-calculator')}}"/>
 		@break
 
 		@case('contact-us')
@@ -53,6 +56,7 @@
 			<meta name="description" content="We are eager to discuss your needs and explore how we can help. Whether it's a new tools or enhancing an existing one, get in touch with us anytime." />
 			<meta property="og:title" content="Contact ToolHubSpot - We're Here to Help You" />
 			<meta property="og:description" content="Have a question or suggestion? Reach out to the ToolHubSpot team anytime. We're here to support you and ensure the best experience with our free online tools." />
+			<link rel="canonical" href="{{url('/contact-us')}}"/>
 		@break
 
 		@case('terms-of-use')
@@ -60,6 +64,7 @@
 			<meta name="description" content="Welcome to {{ config('app.name') }}. These Terms and Conditions govern your use of our services, website, and related tools." />
 			<meta property="og:title" content="ToolHubSpot Terms of Use - Understand Your Rights" />
 			<meta property="og:description" content="Read the official Terms of Use for ToolHubSpot. Learn about user responsibilities, permitted usage, and legal guidelines for accessing our free online tools and services." />
+			<link rel="canonical" href="{{url('/terms-of-use')}}"/>
 		@break
 
 		@case('privacy-policy')
@@ -67,6 +72,7 @@
 			<meta name="description" content="We are a team of IT professionals passionate about delivering IT services and developing high-quality, free tools for public use on the Internet. The primary goal of this website is to offer a comprehensive collection of free online tools (e.g., calculators, color pickers, image resizers, code formatters, etc.) to make everyday tasks easier for users." />
 			<meta property="og:title" content="ToolHubSpot Privacy Policy - Your Data, Our Responsibility" />
 			<meta property="og:description" content="Understand how ToolHubSpot collects, uses, and protects your personal data. We value your privacy and ensure transparency in every step of your interaction with our tools." />
+			<link rel="canonical" href="{{url('/privacy-policy')}}"/>
 		@break
 
 		@case('screen-recording')
@@ -75,6 +81,7 @@
 			<meta property="og:title" content="Free Online Screen Recorder with Audio | No Installation" />
 			<meta property="og:description" content="Record your screen online with ToolHubSpot's free screen recorder. No extensions needed. Just record, preview, and download in a few clicks!" />
 			<meta name="keywords" content="free screen recorder, online screen recording, screen capture tool, screen recording website, no download screen recorder, browser screen recorder, record screen free, screen recording for tutorials, toolhubspot screen recorder">
+			<link rel="canonical" href="{{url('/screen-recording')}}"/>
 		@break
 
 		@case('digital-document')
@@ -82,14 +89,25 @@
 			<meta name="description" content="Create and download professional digital documents in seconds with ToolHubSpot's free custom document generator - perfect for freelancers and businesses." />
 			<meta property="og:title" content="Create Free Digital Document & Download PDF | ToolHubSpot" />
 			<meta property="og:description" content="Create and download professional digital documents in seconds with ToolHubSpot's free custom document generator - perfect for freelancers and businesses.." />
+			<link rel="canonical" href="{{url('/digital-document')}}"/>
 		@break
 
 		@case('custom-invoice')
 			<title>Free Custom Invoice Generator - 100% Editable | ToolHubSpot</title>
-			<meta name="description" content="Generate branded invoices in minutes with our free tool. Add logo, tax, items & download PDF instantly. No login needed – fast, easy, and 100% free!" />
+			<meta name="description" content="Generate branded invoices in minutes with our free tool. Add logo, tax, items & download PDF instantly. No login needed - fast, easy, and 100% free!" />
 			<meta property="og:title" content="Free Custom Invoice Generator - 100% Editable | ToolHubSpot" />
-			<meta property="og:description" content="Generate branded invoices in minutes with our free tool. Add logo, tax, items & download PDF instantly. No login needed – fast, easy, and 100% free!" />
+			<meta property="og:description" content="Generate branded invoices in minutes with our free tool. Add logo, tax, items & download PDF instantly. No login needed - fast, easy, and 100% free!" />
 			<meta name="keywords" content="custom invoice generator, free invoice maker, online invoice tool, invoice template, invoice creator, create invoice online, professional invoice tool, no sign up invoice maker, ToolHubSpot invoice">
+			<link rel="canonical" href="{{url('/custom-invoice')}}"/>
+		@break
+
+		@case('invoice-generator')
+			<title>Free Custom Invoice Generator - 100% Editable | ToolHubSpot</title>
+			<meta name="description" content="Generate branded invoices in minutes with our free tool. Add logo, tax, items & download PDF instantly. No login needed - fast, easy, and 100% free!" />
+			<meta property="og:title" content="Free Custom Invoice Generator - 100% Editable | ToolHubSpot" />
+			<meta property="og:description" content="Generate branded invoices in minutes with our free tool. Add logo, tax, items & download PDF instantly. No login needed - fast, easy, and 100% free!" />
+			<meta name="keywords" content="custom invoice generator, free invoice maker, online invoice tool, invoice template, invoice creator, create invoice online, professional invoice tool, no sign up invoice maker, ToolHubSpot invoice">
+			<link rel="canonical" href="{{url('/custom-invoice')}}"/>
 		@break
 
 		@case('generate-quote')
@@ -98,6 +116,7 @@
 			<meta property="og:title" content="Create Free Quote for Your Clients | ToolHubSpot" />
 			<meta property="og:description" content="Generate professional, branded quotes for your clients with ToolHubSpot's free quote generator. 100% customizable, Download as PDF instantly!" />
 			<meta name="keywords" content="custom quote generator, free quote maker, online quote tool, quote template, quote creator, create quote online, professional quote tool, no sign up quote maker, ToolHubSpot quote">
+			<link rel="canonical" href="{{url('/generate-quote')}}"/>
 		@break
 
 		@case('purchase-order')
@@ -106,6 +125,7 @@
 			<meta property="og:title" content="Create and Download Free Purchase Order | ToolHubSpot" />
 			<meta property="og:description" content="Generate professional purchase orders for free with ToolHubSpot's online PO generator. Fully customizable templates. Download as PDF instantly!" />
 			<meta name="keywords" content="custom purchase order generator, free purchase order maker, online purchase order tool, purchase order template, purchase order creator, create purchase order online, professional purchase order tool, no sign up purchase order maker, ToolHubSpot purchase order">
+			<link rel="canonical" href="{{url('/purchase-order')}}"/>
 		@break
 
 		@case('signature')
@@ -113,6 +133,7 @@
 			<meta name="description" content="Create your digital signature online for free. Draw or type your e-signature and download it as a transparent PNG. Fast, secure, and no registration required." />
 			<meta property="og:title" content="Free Digital Signature Tool - Create Your E-Signature" />
 			<meta property="og:description" content="Create your digital signature in seconds with ToolHubSpot's free online signature tool. Draw, type, or stylize your signature and download instantly!" />
+			<link rel="canonical" href="{{url('/signature')}}"/>
 		@break
 
 		@case('color-picker')
@@ -120,6 +141,7 @@
 			<meta name="description" content="Use our free online Color Picker tool to select and copy any color on your screen. Instantly get HEX, RGB, or HSL codes. Perfect for designers and developers." />
 			<meta property="og:title" content="Free Online Color Picker Tool - Get HEX & RGB Instantly" />
 			<meta property="og:description" content="Use our free online Color Picker tool to select and copy any color on your screen. Instantly get HEX, RGB, or HSL codes. Perfect for designers and developers." />
+			<link rel="canonical" href="{{url('/color-picker')}}"/>
 		@break		
 
 		@default
@@ -127,6 +149,7 @@
 			<meta name="description" content="Explore 100% free online tools on ToolHubSpot - screen recorder, e-signature, timezone converter, create invoice & more. No sign-up, no download, just use!" />
 			<meta property="og:title" content="Free Online Tools for Everyday Work | ToolHubSpot" />
 			<meta property="og:description" content="Explore 100% free online tools on ToolHubSpot - screen recorder, e-signature, timezone converter, create invoice & more. No sign-up, no download, just use!" />
+			<link rel="canonical" href="{{url('/')}}"/>
 		@break
 	@endswitch
 @else
@@ -134,4 +157,5 @@
     <meta name="description" content="Explore 100% free online tools on ToolHubSpot - screen recorder, e-signature, timezone converter, create invoice & more. No sign-up, no download, just use!" />
 	<meta property="og:title" content="Free Online Tools for Everyday Work | ToolHubSpot" />
 	<meta property="og:description" content="Explore 100% free online tools on ToolHubSpot - screen recorder, e-signature, timezone converter, create invoice & more. No sign-up, no download, just use!" />
+	<link rel="canonical" href="{{url('/')}}"/>
 @endif
