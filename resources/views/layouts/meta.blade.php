b26cb3b058c51f58f1109713f586718bf585d41a
unknown <prakashnnpatel@gmail.com>
2025-08-05 20:52:57 +0530

    changes

--- resources/views/layouts/meta.blade.php
+++ resources/views/layouts/meta.blade.php
@@ -21,38 +21,38 @@
 @if(request()->segment(1) !== NULL)
 	@switch(request()->segment(1))
 		@case('timezone')
-			<title>Smart Timezone Converter for Effortless Global Collaboration | ToolHubSpot</title>
+			<title>Smart Timezone Converter | ToolHubSpot</title>
 			<meta name="description" content="The timezone converter tool is powerful & accurate time management, Convert Timezones Instantly and Schedule Meetings Globally with Ease." />
-			<meta property="og:title" content="Free Timezone Converter - Compare Multiple Time Zones Instantly | ToolHubSpot" />
-			<meta property="og:description" content="Easily convert and compare time zones worldwide with ToolHubSpot's free Timezone Converter. Instantly check the current time across cities, plan meetings, and avoid confusion - no sign-up needed!" />
+			<meta property="og:title" content="Smart Timezone Converter | ToolHubSpot" />
+			<meta property="og:description" content="Easily convert and compare time zones worldwide with ToolHubSpot's tool. Instantly check the current time across cities, plan meetings, and avoid confusion!" />
 		@break
 
 		@case('emi-calculator')
-			<title>Free EMI Calculator for Home, Car, and Personal Loans | ToolHubSpot</title>
+			<title>Free EMI Calculator for Home, Car and Personal Loans | ToolHubSpot</title>
 			<meta name="description" content="Use our EMI Calculator to calculate your loan EMIs with ease quickly. Get detailed insights into monthly payments, interest rates, and repayment schedules." />
-			<meta property="og:title" content="Free EMI Calculator - Calculate Loan EMIs Instantly | ToolHubSpot" />
-			<meta property="og:description" content="Easily calculate your monthly loan EMI with ToolHubSpot''s Free EMI Calculator. Get quick results for home, car, personal, or business loans. No login required - simple, fast, and accurate!" />
+			<meta property="og:title" content="Free EMI Calculator - Calculate Loan EMIs | ToolHubSpot" />
+			<meta property="og:description" content="Easily calculate your monthly EMI with ToolHubSpot's Free EMI Calculator. Get quick and accurate results for home, car, personal, or business loans." />
 		@break
 
 		@case('sip-calculator')
-			<title>The Ultimate SIP Calculator to Maximize Your Investments | ToolHubSpot</title>
+			<title>The Smart SIP Calculator to Maximize Your Investments</title>
 			<meta name="description" content="SIP Calculator - Systematic Investment Plan Calculator. This tool can help you determine the amount you want to invest and the estimated value of the returns." />
-			<meta property="og:title" content="Free SIP Calculator - Estimate Mutual Fund Returns Easily | ToolHubSpot" />
-			<meta property="og:description" content="Plan your investments with ToolHubSpot's Free SIP Calculator. Instantly calculate expected returns from Systematic Investment Plans. Easy, accurate & no sign-up needed!" />
+			<meta property="og:title" content="Free SIP Calculator – Plan Your Investments | ToolHubSpot" />
+			<meta property="og:description" content="Plan your investments with ToolHubSpot's Free SIP Calculator. Instantly calculate expected returns from Systematic Investment Plans. Easy, accurate!" />
 		@break
 
 		@case('fd-calculator')
 			<title>FD Calculator - Calculate Fixed Deposit Interest Online</title>
 			<meta name="description" content="Calculate fixed deposit interest easily and maturity value with {{ config('app.name') }} FD Calculator. Plan your savings efficiently with accurate results." />
-			<meta property="og:title" content="Free FD Calculator - Calculate Fixed Deposit Interest Easily | ToolHubSpot" />
-			<meta property="og:description" content="Use ToolHubSpot's Free FD Calculator to estimate your fixed deposit returns. Quick, accurate, and simple - check maturity amounts and interest earned in seconds. No login required!" />
+			<meta property="og:title" content="Free FD Calculator - Calculate FD Interest | ToolHubSpot" />
+			<meta property="og:description" content="Use ToolHubSpot's Free FD Calculator to estimate fixed deposit returns. Quick, accurate, and simple - check maturity amounts and interest earned in seconds." />
 		@break
 
 		@case('contact-us')
 			<title>Have a Question? We're Just a Message Away</title>
 			<meta name="description" content="We are eager to discuss your needs and explore how we can help. Whether it's a new tools or enhancing an existing one, get in touch with us anytime." />
 			<meta property="og:title" content="Contact ToolHubSpot - We're Here to Help You" />
-			<meta property="og:description" content="Have a question, suggestion, or feedback? Reach out to the ToolHubSpot team anytime. We're here to support you and ensure the best experience with our free online tools." />
+			<meta property="og:description" content="Have a question or suggestion? Reach out to the ToolHubSpot team anytime. We're here to support you and ensure the best experience with our free online tools." />
 		@break
 
 		@case('terms-of-use')
@@ -70,68 +70,68 @@
 		@break
 
 		@case('screen-recording')
-			<title>Free Online Screen Recorder with Audio | No Installation | Download Free</title>
+			<title>Free Online Screen Recorder with Audio | No Installation</title>
 			<meta name="description" content="Record your screen with system & mic audio online. No software needed. Free, easy, and secure screen recorder with instant HD download - no watermark!" />
-			<meta property="og:title" content="Free Screen Recording Tool - Record Your Screen & Download Instantly | ToolHubSpot" />
-			<meta property="og:description" content="Capture your screen with ease using ToolHubSpot's free online screen recorder and download instantly. No extensions needed. Record, preview, and save your screen in just a few clicks!" />
+			<meta property="og:title" content="Free Online Screen Recorder with Audio | No Installation" />
+			<meta property="og:description" content="Record your screen online with ToolHubSpot's free screen recorder. No extensions needed. Just record, preview, and download in a few clicks!" />
 			<meta name="keywords" content="free screen recorder, online screen recording, screen capture tool, screen recording website, no download screen recorder, browser screen recorder, record screen free, screen recording for tutorials, toolhubspot screen recorder">
 		@break
 
 		@case('digital-document')
-			<title>Create Free Digital Document & Downloadable PDF | Print Preview | ToolHubSpot</title>
-			<meta name="description" content="Design and download professional digital documents in seconds with ToolHubSpot's Free Custom Document Generator. Whether you're a freelancer, small business owner, or entrepreneur, our tool provides an easy and powerful way to create documents that reflect your brand identity." />
-			<meta property="og:title" content="Create Free Digital Document & Downloadable PDF | Print Preview | ToolHubSpot" />
-			<meta property="og:description" content="Design and download professional digital documents in seconds with ToolHubSpot's Free Custom Document Generator. Whether you're a freelancer, small business owner, or entrepreneur, our tool provides an easy and powerful way to create documents that reflect your brand identity." />
+			<title>Create Free Digital Document & Download PDF | ToolHubSpot</title>
+			<meta name="description" content="Create and download professional digital documents in seconds with ToolHubSpot's free custom document generator - perfect for freelancers and businesses." />
+			<meta property="og:title" content="Create Free Digital Document & Download PDF | ToolHubSpot" />
+			<meta property="og:description" content="Create and download professional digital documents in seconds with ToolHubSpot's free custom document generator - perfect for freelancers and businesses.." />
 		@break
 
 		@case('custom-invoice')
-			<title>Free Custom Invoice Generator - 100% Editable & Downloadable PDF | Print Preview | ToolHubSpot</title>
-			<meta name="description" content="Create professional, branded invoices in minutes with our free custom invoice generator. Fully editable templates with multi-line items, tax options, and your own logo. No login needed, download in PDF format instantly, 100% free and easy to use!" />
-			<meta property="og:title" content="Free Custom Invoice Generator - Create & Download Branded Invoices | ToolHubSpot" />
-			<meta property="og:description" content="Generate branded invoices quickly with ToolHubSpot's free invoice generator. Fully editable templates. Add logo, items, taxes, and download in PDF - no sign-up required!" />
+			<title>Free Custom Invoice Generator - 100% Editable | ToolHubSpot</title>
+			<meta name="description" content="Generate branded invoices in minutes with our free tool. Add logo, tax, items & download PDF instantly. No login needed – fast, easy, and 100% free!" />
+			<meta property="og:title" content="Free Custom Invoice Generator - 100% Editable | ToolHubSpot" />
+			<meta property="og:description" content="Generate branded invoices in minutes with our free tool. Add logo, tax, items & download PDF instantly. No login needed – fast, easy, and 100% free!" />
 			<meta name="keywords" content="custom invoice generator, free invoice maker, online invoice tool, invoice template, invoice creator, create invoice online, professional invoice tool, no sign up invoice maker, ToolHubSpot invoice">
 		@break
 
 		@case('generate-quote')
-			<title>Free Quote Generator for Clients - Create & Download Custom Quotes | ToolHubSpot</title>
-			<meta name="description" content="Generate professional, branded quotes for your clients with ToolHubSpot's free quote generator. 100% customizable, no sign-up required. Download as PDF instantly!" />
-			<meta property="og:title" content="Free Quote Generator - Create Client Quotes Online | ToolHubSpot" />
-			<meta property="og:description" content="Easily create and download professional quotes with ToolHubSpot's free quote generator. Fully customizable for your business - fast, simple, and free to use!" />
+			<title>Create Free Quote for Your Clients | ToolHubSpot</title>
+			<meta name="description" content="Generate professional, branded quotes for your clients with ToolHubSpot's free quote generator. 100% customizable, Download as PDF instantly!" />
+			<meta property="og:title" content="Create Free Quote for Your Clients | ToolHubSpot" />
+			<meta property="og:description" content="Generate professional, branded quotes for your clients with ToolHubSpot's free quote generator. 100% customizable, Download as PDF instantly!" />
 			<meta name="keywords" content="custom quote generator, free quote maker, online quote tool, quote template, quote creator, create quote online, professional quote tool, no sign up quote maker, ToolHubSpot quote">
 		@break
 
 		@case('purchase-order')
-			<title>Free Purchase Order Generator - Create & Download Custom POs Online | ToolHubSpot</title>
-			<meta name="description" content="Generate professional purchase orders for free with ToolHubSpot's online PO generator. Fully customizable templates. No sign-up required. Download as PDF instantly!" />
-			<meta property="og:title" content="Free Purchase Order Generator - Create Custom POs | ToolHubSpot" />
-			<meta property="og:description" content="Generate professional purchase orders instantly with ToolHubSpot's free PO generator. Customize supplier info, items, pricing, and download in PDF format - no login needed!" />
+			<title>Create and Download Free Purchase Order | ToolHubSpot</title>
+			<meta name="description" content="Generate professional purchase orders for free with ToolHubSpot's online PO generator. Fully customizable templates. Download as PDF instantly!" />
+			<meta property="og:title" content="Create and Download Free Purchase Order | ToolHubSpot" />
+			<meta property="og:description" content="Generate professional purchase orders for free with ToolHubSpot's online PO generator. Fully customizable templates. Download as PDF instantly!" />
 			<meta name="keywords" content="custom purchase order generator, free purchase order maker, online purchase order tool, purchase order template, purchase order creator, create purchase order online, professional purchase order tool, no sign up purchase order maker, ToolHubSpot purchase order">
 		@break
 
 		@case('signature')
 			<title>Free Digital Signature Tool - Create Your E-Signature</title>
 			<meta name="description" content="Create your digital signature online for free. Draw or type your e-signature and download it as a transparent PNG. Fast, secure, and no registration required." />
-			<meta property="og:title" content="Free Digital Signature Generator - Create Your Signature Online and Download | ToolHubSpot" />
-			<meta property="og:description" content="Create your digital signature in seconds with ToolHubSpot's free online signature tool. Draw, type, or stylize your signature and download instantly - no sign-up required!" />
+			<meta property="og:title" content="Free Digital Signature Tool - Create Your E-Signature" />
+			<meta property="og:description" content="Create your digital signature in seconds with ToolHubSpot's free online signature tool. Draw, type, or stylize your signature and download instantly!" />
 		@break
 
 		@case('color-picker')
-			<title>Free Online Color Picker Tool - Get HEX, RGB & HSL Instantly</title>
+			<title>Free Online Color Picker Tool - Get HEX & RGB Instantly</title>
 			<meta name="description" content="Use our free online Color Picker tool to select and copy any color on your screen. Instantly get HEX, RGB, or HSL codes. Perfect for designers and developers." />
-			<meta property="og:title" content="Free Color Picker Tool - Find & Copy HEX, RGB, HSL Codes | ToolHubSpot" />
-			<meta property="og:description" content="Pick any color and instantly get its HEX, RGB, and HSL values with ToolHubSpot's free color picker tool. Perfect for designers and developers - no installation needed!" />
+			<meta property="og:title" content="Free Online Color Picker Tool - Get HEX & RGB Instantly" />
+			<meta property="og:description" content="Use our free online Color Picker tool to select and copy any color on your screen. Instantly get HEX, RGB, or HSL codes. Perfect for designers and developers." />
 		@break		
 
 		@default
-			<title>Free Online Tools for Everyday Work - Screen Recorder, Timezone Converter & More | ToolHubSpot</title>
-			<meta name="description" content="Discover 100% free and easy-to-use online tools at ToolHubSpot - including screen recorder, digital signature creator, timezone converter, color picker, and more. No downloads, no sign-up. Boost productivity in seconds!" />
-			<meta property="og:title" content="Free Online Tools for Everyday Work - Screen Recorder, Timezone Converter & More | ToolHubSpot" />
-			<meta property="og:description" content="Discover 100% free and easy-to-use online tools at ToolHubSpot - including screen recorder, digital signature creator, timezone converter, color picker, and more. No downloads, no sign-up. Boost productivity in seconds!" />
+			<title>Free Online Tools for Everyday Work | ToolHubSpot</title>
+			<meta name="description" content="Explore 100% free online tools on ToolHubSpot - screen recorder, e-signature, timezone converter, create invoice & more. No sign-up, no download, just use!" />
+			<meta property="og:title" content="Free Online Tools for Everyday Work | ToolHubSpot" />
+			<meta property="og:description" content="Explore 100% free online tools on ToolHubSpot - screen recorder, e-signature, timezone converter, create invoice & more. No sign-up, no download, just use!" />
 		@break
 	@endswitch
 @else
-	<title>Free Online Tools for Everyday Work - Screen Recorder, Timezone Converter & More | ToolHubSpot</title>
-    <meta name="description" content="Discover 100% free and easy-to-use online tools at ToolHubSpot - including screen recorder, digital signature creator, timezone converter, color picker, and more. No downloads, no sign-up. Boost productivity in seconds!" />
-	<meta property="og:title" content="Free Online Tools for Everyday Work - Screen Recorder, Timezone Converter & More | ToolHubSpot" />
-	<meta property="og:description" content="Discover 100% free and easy-to-use online tools at ToolHubSpot - including screen recorder, digital signature creator, timezone converter, color picker, and more. No downloads, no sign-up. Boost productivity in seconds!" />
+	<title>Free Online Tools for Everyday Work | ToolHubSpot</title>
+    <meta name="description" content="Explore 100% free online tools on ToolHubSpot - screen recorder, e-signature, timezone converter, create invoice & more. No sign-up, no download, just use!" />
+	<meta property="og:title" content="Free Online Tools for Everyday Work | ToolHubSpot" />
+	<meta property="og:description" content="Explore 100% free online tools on ToolHubSpot - screen recorder, e-signature, timezone converter, create invoice & more. No sign-up, no download, just use!" />
 @endif
