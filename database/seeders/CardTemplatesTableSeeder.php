448a020122b8e7a5bff73cd56cc02845a656798c
unknown <prakashnnpatel@gmail.com>
2025-09-23 18:24:00 +0530

    Changes

--- database/seeders/CardTemplatesTableSeeder.php
+++ database/seeders/CardTemplatesTableSeeder.php
@@ -144,7 +144,7 @@ class CardTemplatesTableSeeder extends Seeder
                         [
                             'type' => 'text',
                             'id' => 'title2',
-                            'text' => 'Any Good Name here',
+                            'text' => 'Add Good Name here',
                             'css' => 'z-index: 3;left: 174px;top: 294px;font-size: 18px;font-style: italic;border-color: rgb(191, 196, 223);border-width: 3px;border-style: none;background-color: rgb(216, 218, 233);',
                         ],
                         [
@@ -163,7 +163,7 @@ class CardTemplatesTableSeeder extends Seeder
                             'type' => 'image',
                             'id' => 'img2',
                             'src' => '/images/greeting-cards/element/kid-1.png',
-                            'css' => 'position: absolute;top: 151px;left: 253px;width: 126px;height: 115.623px;border: 5px double rgb(165, 126, 151);cursor: move;z-index: 38;transform: rotate(-6.90027deg);',
+                            'css' => 'position: absolute; top: 151px; left: 253px; width: 126px; height: 115.623px; border: 5px double rgb(165, 126, 151); cursor: move; z-index: 8; transform: rotate(-6.90027deg); border-radius: 50%',
                         ], 
                         [
                             'type' => 'image',
@@ -171,6 +171,84 @@ class CardTemplatesTableSeeder extends Seeder
                             'src' => '/images/greeting-cards/element/kid-3.png',
                             'css' => 'position: absolute;top: 205px;left: 372px;width: 139.995px;height: 124.085px;border: 2px outset rgb(209, 128, 128);cursor: move;z-index: 21;transform: rotate(-7.87307deg);',
                         ],                        
+                    ]
+                ]),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'title' => 'Celebration Glow',
+                'slug' => 'celebration-glow',
+                'thumbnail' => '/images/greeting-cards/celebration-glow-thumb.png',
+                'category' => 'Birthday',
+                'description' => 'A colorful birthday card with balloons.',
+                'template_data' => json_encode([
+                    'bgColor' => '',
+                    'bgImage' => "/images/greeting-cards/backgrounds/birthday-11.jpg",
+                    'elements' => [
+                        [
+                            'type' => 'text',
+                            'id' => 'title1',
+                            'text' => 'Happy Birthday',
+                            'css' => 'z-index: 36; left: 190px; top: 134px; font-family: Lobster, sans-serif; font-size: 38px; color: rgb(255, 255, 255); transform: rotate(-0.320775deg); background: linear-gradient(90deg, rgb(93 202 85) 0%, rgb(14 68 39) 100%);',
+                        ],
+                        [
+                            'type' => 'text',
+                            'id' => 'title2',
+                            'text' => 'Friend Name',
+                            'css' => 'z-index: 47; left: 212px; top: 216px; color: #000000;font-family: Lobster, sans-serif; font-size: 35px;',
+                        ],
+                        [
+                            'type' => 'text',
+                            'id' => 'title3',
+                            'text' => 'Have a Wonderful Day',
+                            'css' => 'z-index: 47; left: 198px; top: 322px; color: rgb(6, 127, 58); font-weight: bold;',
+                        ],
+                    ]
+                ]),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'title' => 'Twins Birthday',
+                'slug' => 'twins-birthday',
+                'thumbnail' => '/images/greeting-cards/twins-birthday-thumb.png',
+                'category' => 'Birthday',
+                'description' => 'A colorful birthday card with balloons.',
+                'template_data' => json_encode([
+                    'bgColor' => '',
+                    'bgImage' => "/images/greeting-cards/backgrounds/birthday-9.jpg",
+                    'elements' => [
+                        [
+                            'type' => 'text',
+                            'id' => 'title1',
+                            'text' => 'Happy Birthday',
+                            'css' => 'z-index: 62; left: 54px; top: 328px; font-family: Lobster, sans-serif; font-size: 36px; color: rgb(49, 66, 10); transform: rotate(-0.320775deg);',
+                        ],
+                        [
+                            'type' => 'text',
+                            'id' => 'title2',
+                            'text' => 'Vansh',
+                            'css' => 'z-index: 64; left: 164px; top: 215px; color: rgb(108, 121, 42); font-family: Lobster, sans-serif; font-size: 35px; transform: rotate(-0.165558deg);',
+                        ],
+                        [
+                            'type' => 'text',
+                            'id' => 'title3',
+                            'text' => 'Ved',
+                            'css' => 'z-index: 65; left: 364px; top: 128px; color: rgb(108, 121, 42); font-family: Lobster, sans-serif; font-size: 35px;',
+                        ],
+                        [
+                            'type' => 'image',
+                            'id' => 'img2',
+                            'src' => '/images/greeting-cards/element/kid-2.png',
+                            'css' => 'position: absolute; top: 36px; left: 127px; width: 186.993px; height: 173.925px; border: 4px solid rgb(221, 169, 120); cursor: move; z-index: 66; border-radius: 50%; transform: rotate(-0.217921deg);',
+                        ],
+                        [
+                            'type' => 'image',
+                            'id' => 'img1',
+                            'src' => '/images/greeting-cards/element/kid-1.png',
+                            'css' => 'position: absolute; top: 197px; left: 313px; width: 190.988px; height: 173.602px; border: 5px solid rgb(174, 163, 163); cursor: move; z-index: 56; border-radius: 50%; transform: rotate(-0.667781deg);',
+                        ],
                     ]
                 ]),
                 'created_at' => now(),
